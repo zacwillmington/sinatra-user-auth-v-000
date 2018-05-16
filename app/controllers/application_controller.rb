40c011df09b6ac12bd11cadc593a2412b051451e
Zac Willmington <zac.willmington@gmail.com>
2018-05-16 15:30:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-      @user = User.find(session[:id])
+    #   @user = User.find(session[:id])
     redirect '/users/home'
   end
 
